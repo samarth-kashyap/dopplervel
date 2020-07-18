defebb8db373389d1919633f06b5d3b99d8c0664
Samarth Kashyap <samarth.g.kashyap@outlook.com>
2020-07-18 10:40:32 +0530

    data_analysis complete

--- rotation.py
+++ rotation.py
@@ -1,39 +1,29 @@
+# {{{ Library imports
 from scipy.spatial.transform import Rotation as R
-from pyshtools import legendre as pleg     # Legendre polynomials
-from scipy.integrate import simps          # Integration - simpsons
-import matplotlib.pyplot as plt            # Plotting
-from math import sqrt, pi, e               # Math constants
-from astropy.io import fits                # FITS handling
-import astropy.units as u                  # Handling qty with units
+from globalvars import DopplerVars
+import matplotlib.pyplot as plt
+from math import pi
 import healpy as hp
 import numpy as np
 import argparse
 import time
-import os
+# }}} imports
 
+
+# {{{ Argument Parser
 parser = argparse.ArgumentParser()
-parser.add_argument('--hpc', help="Run program on daahpc",
-                    action="store_true")
-parser.add_argument('--cchpc', help="Run program on cchpc19",
-                    action="store_true")
 parser.add_argument('--gnup', help="Argument for GNU Parallel",
                     type=int)
 args = parser.parse_args()
+gvar = DopplerVars(args.gnup)
+# }}} parser
 
-if args.hpc:
-    home_dir = "/home/samarth/"
-    scratch_dir = "/scratch/samarth/"
-if args.cchpc:
-    home_dir = "/home/g.samarth/"
-    scratch_dir = "/scratch/g.samarth/"
-else:
-    home_dir = "/home/samarthgk/cchpchome/"
-    scratch_dir = "/home/samarthgk/cchpcscratch/"
 
+# {{{ def alm2almhp(alm, ellArr, emmArr, lmax):
 def alm2almhp(alm, ellArr, emmArr, lmax):
     '''Converts the spectral coefficients array from the pyshtools format
     to the healPy format.
-    
+
     Parameters:
     -----------
     alm - np.ndarray(ndim=1, dtype=complex)
@@ -54,28 +44,46 @@ def alm2almhp(alm, ellArr, emmArr, lmax):
     _maxind = int((lmax+1)*(lmax+2)/2)
     alm = alm[:_maxind].copy()
     almhp = np.array([], dtype=complex)
-    within_max = ellArr<=lmax
+    within_max = ellArr <= lmax
     for emm in range(lmax+1):
-        isem = emmArr==emm
+        isem = emmArr == emm
         mask = within_max * isem
         almhp = np.append(almhp, alm[mask])
     return almhp
+# }}} alm2almhp(alm, ellArr, emmArr, lmax)
 
+
+# {{{ def rotate_map_spin_eul(hmap, eulAngle):
 def rotate_map_spin_eul(hmap, eulAngle):
-    """Take hmap (a healpix map array) and return another healpix map array 
-    which is ordered such that it has been rotated in (theta, phi) by the 
+    """Take hmap (a healpix map array) and return another healpix map array
+    which is ordered such that it has been rotated in (theta, phi) by the
     amounts given.
+
+    Parameters:
+    -----------
+    hmap - healPy map list
+        hmap[0] - healPy map corresponding to theta
+        hmap[1] - healPy map corresponding to phi
+    eulAngle - euler Angle
+
+    Returns:
+    --------
+    (rot_map0, rot_map1)
+    rot_map0 - healPy map
+        rotated map corresponding to theta
+    rot_map1 - healPy map
+        rotated map corresponding to phi
     """
     npix = len(hmap[0])
     nside = hp.npix2nside(npix)
 
     # Get theta, phi for non-rotated map
-    theta, phi = hp.pix2ang(nside, np.arange(npix)) 
+    theta, phi = hp.pix2ang(nside, np.arange(npix))
     costh, cosph = np.cos(theta), np.cos(phi)
     sinth, sinph = np.sin(theta), np.sin(phi)
     vth = np.array([costh*cosph, costh*sinph, -sinth])
     vph = np.array([-sinph, cosph, 0.0*cosph])
-    
+
     r = hp.rotator.Rotator(eulAngle, deg=False, eulertype='zxz')
 
     # Get theta, phi under rotated co-ordinates
@@ -85,23 +93,26 @@ def rotate_map_spin_eul(hmap, eulAngle):
     vth_rot = np.array([costh_rot*cosph_rot, costh_rot*sinph_rot, -sinth_rot])
     vph_rot = np.array([-sinph_rot, cosph_rot, 0.0*cosph_rot])
 
-    rotMat = R.from_euler('zxz', eulAngle).as_dcm()
-    vth_rot = vth_rot.transpose().dot(rotMat).transpose()
-    vph_rot = vph_rot.transpose().dot(rotMat).transpose()
+    rot_mat = R.from_euler('zxz', eulAngle).as_dcm()
+    vth_rot = vth_rot.transpose().dot(rot_mat).transpose()
+    vph_rot = vph_rot.transpose().dot(rot_mat).transpose()
 
     # Interpolate map onto these co-ordinates
     rot_map0temp = hp.get_interp_val(hmap[0], theta_rot, phi_rot)
     rot_map1temp = hp.get_interp_val(hmap[1], theta_rot, phi_rot)
 
     # Obtaining the rotated maps
-    rot_map0 = (vth*vph_rot).sum(axis=0) * rot_map0temp \
-             + (vph*vph_rot).sum(axis=0) * rot_map1temp
+    rot_map0 = ((vth*vph_rot).sum(axis=0) * rot_map0temp +
+                (vph*vph_rot).sum(axis=0) * rot_map1temp)
 
-    rot_map1 = (vth*vth_rot).sum(axis=0) * rot_map0temp \
-             + (vph*vth_rot).sum(axis=0) * rot_map1temp
+    rot_map1 = ((vth*vth_rot).sum(axis=0) * rot_map0temp +
+                (vph*vth_rot).sum(axis=0) * rot_map1temp)
 
     return rot_map0, rot_map1
+# }}} rotate_map_spin_eul(hmap, eulAngle)
 
+
+# {{{ def get_spin1_alms(map_r, map_trans):
 def get_spin1_alms(map_r, map_trans):
     """Get the vector spherical harmonic coefficients for spin1 harmonics.
 
@@ -113,13 +124,13 @@ def get_spin1_alms(map_r, map_trans):
         len(map_trans) = 2
         map_trans[0] - map of vector field corresponding to +1 component
         map_trans[1] - map of vector field corresponding to -1 component
-    
+
     Returns:
     --------
     alm2r - spin0 spherical harmonic coefficients
     alm2v - spin1 spherical harmonic coefficients for s=+1
     alm2w - spin1 spherical harmonic coefficients for s=-1
-    
+
     """
     assert len(map_r) == len(map_trans[0]) == len(map_trans[1])
     alm_r = hp.map2alm(map_r)
@@ -127,10 +138,13 @@ def get_spin1_alms(map_r, map_trans):
     alm_v = -alm_pm[0]
     alm_w = -1j*alm_pm[1]
     return alm_r, alm_v, alm_w
+# }}} get_spin1_alms(map_r, map_trans)
 
+
+# {{{ def computePS(ellArr, emmArr, lmax, coefs):
 def computePS(ellArr, emmArr, lmax, coefs):
     """Computes the power spectrum
-    
+
     Parameters:
     -----------
     ellArr - np.ndarray(ndim=1, dtype=int)
@@ -149,21 +163,24 @@ def computePS(ellArr, emmArr, lmax, coefs):
 
     Notes:
     ------
-    Power spectrum = \sum_{m} | alm |^2 
+    Power spectrum = sum_{m} | alm |^2
 
     """
     ps = np.zeros(lmax+1)
     for ell in range(lmax+1):
-        isel = ellArr==ell
-        ps[ell] = ell * (abs(coefs[isel])**2).sum()# / (2*ell+1)
+        isel = ellArr == ell
+        ps[ell] = ell * (abs(coefs[isel])**2).sum()  # / (2*ell+1)
     return np.sqrt(ps)
+# }}} computePS(ellArr, emmArr, lmax, coefs)
 
+
+# {{{ def get_ell_emm_arr(lmax):
 def get_ell_emm_arr(lmax):
     '''Returns the ellArr and emmArr arrays for the pyshtools convention.
-    
+
     Parameters:
     -----------
-    lmax - int 
+    lmax - int
         maximum spherical harmonic degree
 
     Returns:
@@ -181,10 +198,13 @@ def get_ell_emm_arr(lmax):
             ellArr = np.append(ellArr, ell)
             emmArr = np.append(emmArr, emm)
     return ellArr, emmArr
+# }}} get_ell_emm_arr(lmax)
 
+
+# {{{ def alm4nside(ulm, vlm, wlm, ellArr, emmArr, nside):
 def alm4nside(ulm, vlm, wlm, ellArr, emmArr, nside):
     '''Get alm for given nside
-    
+
     Parameters:
     -----------
     ulm - np.ndarray(ndim=1, dtype=complex)
@@ -194,11 +214,11 @@ def alm4nside(ulm, vlm, wlm, ellArr, emmArr, nside):
     wlm - np.ndarray(ndim=1, dtype=complex)
         toroidal harmonic coefficients
     ellArr - np.ndarray(ndim=1, dtype=int)
-        array containing ell 
+        array containing ell
     emmArr - np.ndarray(ndim=1, dtype=int)
         array containing emm
-    nside - int 
-        NSIDE parameter of the healPy map 
+    nside - int
+        NSIDE parameter of the healPy map
 
     Returns:
     --------
@@ -209,20 +229,21 @@ def alm4nside(ulm, vlm, wlm, ellArr, emmArr, nside):
     wlm - np.ndarray(ndim=1, dtype=complex)
         toroidal harmonic coefficients
     ellArr - np.ndarray(ndim=1, dtype=int)
-        array containing ell 
+        array containing ell
     emmArr - np.ndarray(ndim=1, dtype=int)
         array containing emm
-    
+
     '''
     lmax4nside = 3*nside - 1
-    maskell = ellArr<=lmax4nside
+    maskell = ellArr <= lmax4nside
     ellArr = ellArr[maskell].copy()
     emmArr = emmArr[maskell].copy()
     ulm = ulm[maskell].copy()
     vlm = vlm[maskell].copy()
     wlm = wlm[maskell].copy()
-
     return ulm, vlm, wlm, ellArr, emmArr
+# }}} alm4nside(ulm, vlm, wlm, ellArr, emmArr, nside)
+
 
 if __name__=="__main__":
     workingDir = scratch_dir + "matrixA/"
@@ -252,7 +273,6 @@ if __name__=="__main__":
 
     ulm2, vlm2, wlm2, ellArr, emmArr = alm4nside(ulm2, vlm2, wlm2, ellArr,
                                                  emmArr, NSIDE)
-
 
     map2r = hp.alm2map(ulm2, NSIDE)
     map2trans = hp.alm2map_spin((-vlm2, 1j*wlm2), NSIDE, SPIN, LMAXHP)
